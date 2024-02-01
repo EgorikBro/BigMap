9be692c41f82a35750e244e50ef03c131d4beba2
nazarenko <nazarenkoe106@gmail.com>
2024-02-01 19:41:02 +0500

    4 задача

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from io import BytesIO
 
 import pygame
+import pygame_gui
 
 from api_library import *
 
@@ -24,18 +25,24 @@ def load_image(name, colorkey=None):
 
 
 class BigMap:
+    options = ['map', 'sat', 'sat,skl']
+
     def __init__(self):
         self.image = None
         self.lon, self.lat = 60.153191, 55.156353
         self.layer = 'map'
-        self.z = 17
+        self.z = 15
+        self.manager = pygame_gui.UIManager(SIZE)
+        self.layers_select = (pygame_gui.elements.
+                              UIDropDownMenu(self.options, self.options[0],
+                                             pygame.Rect(10, 10, 200, 30), self.manager))
         self.update_map()
 
     def update_map(self):
         map_params = {
             "ll": ",".join(map(str, (self.lon, self.lat))),
             'z': self.z,
-            "l": "map",
+            "l": self.layer,
             'size': '650,450'
         }
         image = BytesIO(get_static(**map_params))
@@ -56,9 +63,20 @@ class BigMap:
             if event.key == pygame.K_DOWN:
                 self.lat = max(self.lat - 70 * 2 ** (-self.z), -88)
             self.update_map()
+        self.manager.process_events(event)
+
+    def gui_event_handler(self, event):
+        if event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+            if event.ui_element == self.layers_select:
+                self.layer = event.text
+                self.update_map()
 
     def draw(self, surf):
         surf.blit(self.image, (0, 0))
+        self.manager.draw_ui(surf)
+
+    def update_gui(self, delta):
+        self.manager.update(delta)
 
 
 if __name__ == '__main__':
@@ -67,13 +85,16 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(SIZE)
     app = BigMap()
 
+    clock = pygame.time.Clock()
     running = True
     while running:
+        time_delta = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             app.event_handler(event)
-
+            app.gui_event_handler(event)
+        app.update_gui(time_delta)
         screen.fill('black')
         app.draw(screen)
         pygame.display.flip()
