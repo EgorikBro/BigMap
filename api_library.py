b9a95bf80ce412d37f6e92cde4410e5053e986c5
nazarenko <nazarenkoe106@gmail.com>
2024-02-01 18:23:44 +0500

    1 задача

---
+++ api_library.py
@@ -0,0 +1,80 @@
+import math
+
+import requests
+
+
+def get_static(**params):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=params)
+    return response.content
+
+
+def geocode(**params):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    response = requests.get(geocoder_api_server, params=params)
+
+    if not response:
+        print('Error', response.reason, response.status_code)
+        exit(-1)
+
+    return response.json()
+
+
+def get_toponym(json_data):
+    return json_data["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+
+
+def get_toponym_coord(toponym):
+    return list(map(float, toponym['Point']['pos'].split(" ")))
+
+
+def get_spn(toponym):
+    lon1, lat1 = map(float, toponym['boundedBy']['Envelope']["lowerCorner"].split())
+    lon2, lat2 = map(float, toponym['boundedBy']['Envelope']["upperCorner"].split())
+    return lon2 - lon1, lat2 - lat1
+
+
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
+
+
+def search_organization(**params):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+    params['apikey'] = api_key
+    response = requests.get(search_api_server, params=params)
+
+    if not response:
+        print('Error', response.reason, response.status_code)
+        exit(-1)
+
+    return response.json()
+
+
+def get_organizations(json_data):
+    return json_data["features"]
+
+
+def get_org_info(organization):
+    org_name = organization["properties"]["CompanyMetaData"]["name"]
+    org_address = organization["properties"]["CompanyMetaData"]["address"]
+    org_hours = organization["properties"]["CompanyMetaData"]["Hours"]['text']
+    return org_name, org_address, org_hours
+
+
+def get_organization_coordinates(organization):
+    point = organization["geometry"]["coordinates"]
+    return point
